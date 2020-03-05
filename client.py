9a5a6d23d73eabb4fe31930073015169ae4a81d4
1Samper1 <31518175+Samepr@users.noreply.github.com>
2020-03-05 10:57:03 +0100

    Client qui "fonctionne"

--- client.py
+++ client.py
@@ -1,25 +1,73 @@
+import sys
+import random
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+                                                    
+#Connexion au serveur
 import socket
+import select
 
 hote = 'localhost'
-port = 12800
+port = 25565
 
-# On définit le socket pour une connexion TCP
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# On se connecte au serveur
 connexion_avec_serveur.connect((hote, port))
 print("La connexion avec le serveur a été établie sur le port", port)
+#Fin de la connexion
 
-msg_envoyé = b""
-while msg_envoyé != b"fin":
-    msg_envoyé = input("> ")
-    msg_envoyé = msg_envoyé.encode()
+class Cryptenger(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
 
-    connexion_avec_serveur.send(msg_envoyé)
+        self.historique = ''
+ 
+        self.button = QPushButton("Send Message")
+        self.refresh = QPushButton("Rafraîchir")
+        self.dialogue = QLineEdit()
+        self.messages = QTextEdit()
+        
+        self.button.clicked.connect(self.msgSend)
+        self.refresh.clicked.connect(self.msgRecv)
+        self.messages.setReadOnly(True)
 
-    #Attente de la réponse du serveur
-    msg_reçu = connexion_avec_serveur.recv(1024)
-    print(msg_reçu.decode())
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.messages)
+        self.layout.addWidget(self.dialogue)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.refresh)
+        self.setLayout(self.layout)
 
-# On ferme la connexion avec le serveur
-print("Fermeture de la connexion")
-connexion_avec_serveur.close()
+    def msgSend(self):
+        if self.dialogue.text() != '':
+            #self.historique = self.historique + '\n' + self.dialogue.text()
+            #self.messages.setPlainText(self.historique)
+            connexion_avec_serveur.send(self.dialogue.text().encode())
+            self.dialogue.setText('')
+
+            if self.dialogue.text() == "fin":
+                QCoreApplication.instance().quit
+
+    def msgRecv(self):
+        message_serveur, wlist, xlist = select.select([connexion_avec_serveur], [], [], 0.05)
+
+        if len(message_serveur) != 0:
+            # Client est de type socket
+            msg_recu = message_serveur[0].recv(1024)
+            # Peut planter si le message contient des caractères spéciaux
+            msg_recu = msg_recu.decode()
+            self.historique = self.historique + msg_recu + '\n'
+            self.messages.setPlainText(self.historique)
+
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    
+    widget = Cryptenger()
+    widget.show()
+
+
+
+    sys.exit(app.exec())
